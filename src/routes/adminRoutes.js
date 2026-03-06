d27cb08dee9ee8587bf94e92be0764b763afd0da
Gribuz <gribuzcode@gmail.com>
2026-03-05 21:21:39 -0600

    Se agregan los archivos y metodos para el CRUD

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const { login } = require('../controllers/authController');
+const { crearExamen, actualizarExamen, eliminarExamen } = require('../controllers/adminExamenController');
 
 
 //Ruta del login pa
 router.post('/login', login);
+
+// CRUD de Exámenes ETS
+router.post('/examenes', crearExamen);          // Alta
+router.put('/examenes/:id', actualizarExamen);  // Cambio
+router.delete('/examenes/:id', eliminarExamen); // Baja
 
 module.exports = router;
